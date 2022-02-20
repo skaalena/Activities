e34fd7daa641eceae45b89cd57352e25825a4982
skaalena <98474452+skaalena@users.noreply.github.com>
2022-02-20 08:22:36 -0600

    Update activity4a.py

--- Activity4/activity4a.py
+++ Activity4/activity4a.py
@@ -1,1 +1,22 @@
+# Activity 4- Python List
+# Autumn Skaalen
+# ISQA 3900 Web Application Developement
+# February 20, 2022
+# List duplicate remover
 
+# activity 4a
+
+def Remove(names):
+    print("Initial List of Names\n ['mary','mary','bill','sam','maria','kahn','bill','barry','george','hank','belinda','maria','karthik']\n")
+
+    print("List of unique names after running through the de-duplicator program")
+
+    final_list = []
+    for num in names:
+        if num not in final_list:
+            final_list.append(num)
+            return final_list
+
+            names = ['mary','mary','bill','sam','maria','kahn','bill','barry','george','hank','belinda','maria','karthik']
+
+            print(Remove(names))
